4df1041ec4576371dab1c982b1bb7740af3eb687
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-01-03 20:56:26 +0800

    Add container, divs

---
+++ index.js
@@ -0,0 +1,67 @@
+const container = document.getElementById("container");
+
+const div1 = document.createElement("div");
+container.appendChild(div1);
+
+const div2 = document.createElement("div");
+container.appendChild(div2);
+
+const div3 = document.createElement("div");
+container.appendChild(div3);
+
+const div4 = document.createElement("div");
+container.appendChild(div4);
+
+const div5 = document.createElement("div");
+container.appendChild(div5);
+
+const div6 = document.createElement("div");
+container.appendChild(div6);
+
+const div7 = document.createElement("div");
+container.appendChild(div7);
+
+const div8 = document.createElement("div");
+container.appendChild(div8);
+
+const div9 = document.createElement("div");
+container.appendChild(div9);
+
+const div10 = document.createElement("div");
+container.appendChild(div10);
+
+const div11 = document.createElement("div");
+container.appendChild(div11);
+
+const div12 = document.createElement("div");
+container.appendChild(div12);
+
+const div13 = document.createElement("div");
+container.appendChild(div13);
+
+const div14 = document.createElement("div");
+container.appendChild(div14);
+
+const div15 = document.createElement("div");
+container.appendChild(div15);
+
+const div16 = document.createElement("div");
+container.appendChild(div16);
+
+//creating mouse hover
+
+/*
+
+const button = document.createElement('button');
+container.appendChild (button);
+
+button.addEventListener('mouseover', () => {
+    button.style.backgroundColor = "blue";
+})
+
+button.addEventListener('mouseout', () => {
+    button.style.backgroundColor = '';
+})
+
+
+*/
